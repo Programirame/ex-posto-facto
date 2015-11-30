e3f9fa641c19537760911840b8f8f893a16389e0
igstojanovski <igor.stojanovski@endava.com>
2015-11-30 19:58:31 +0100

    Initial Commit
    Basic Spring Boot Setup

---
+++ src/main/java/org/programirame/RetrospectiveServicesApplication.java
@@ -0,0 +1,12 @@
+package org.programirame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospectiveServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrospectiveServicesApplication.class, args);
+    }
+}
