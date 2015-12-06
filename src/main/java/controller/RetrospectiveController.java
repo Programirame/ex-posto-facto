d4954b7671410948ea4470544bbfcad430b4cd39
igstojanovski <igor.stojanovski@endava.com>
2015-12-06 12:53:08 +0100

    RetrospectiveController

--- src/main/java/controller/RetrospectiveController.java
+++ src/main/java/controller/RetrospectiveController.java
@@ -1,7 +1,14 @@
 package controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RetrospectiveController {
+
+    public ResponseEntity<?> createRetrospective() {
+        return new ResponseEntity<Object>(null, HttpStatus.OK);
+    }
+
 }
