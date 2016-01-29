15a21b07b6a631423ad84aed4c512a3a6d82ef27
igorstojanovski <igorce@outlook.com>
2016-01-29 09:55:29 +0100

    Application builds as a war
    App builds as an executable war. Changes made to the pom to exclude the
    embedded tomcat.

--- src/main/java/com/ex/post/facto/Application.java
+++ src/main/java/com/ex/post/facto/Application.java
@@ -2,15 +2,19 @@ package com.ex.post.facto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {  
+public class Application extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ExampleApplication.class);
+        return application.sources(Application.class);
     }
+
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
